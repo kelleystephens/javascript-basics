336f0e39b45b0f27ee0ee472d70135d2ba7c0071
Kelley Stephens <kelley.stephens@outlook.com>
2014-04-08 16:01:30 -0500

    basic js

---
+++ main.js
@@ -0,0 +1,11 @@
+//this is a comment
+
+/*this is a
+  multiline comment
+  */
+
+var firstName = prompt('What is your first name?');
+var lastName = prompt('What is your last name?');
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
